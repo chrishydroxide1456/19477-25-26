cda7c4833d4a318822b90fa415eefd23a2e6d7ab
chrishydroxide1456 <chrisoh2026@gmail.com>
2025-12-12 07:57:05 -0500

    init problem should be fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -17,11 +17,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(11.2)
-            .forwardZeroPowerAcceleration(-27.8)
-            .lateralZeroPowerAcceleration(-72.3)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.08, 0, 0.007, 0.01))
-            .headingPIDFCoefficients(new PIDFCoefficients(1.2, 0, 0.09, 0.01))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.004, 0, 0.001, 0.7, 0.0))
+            .forwardZeroPowerAcceleration(-34.790656451320785)
+            .lateralZeroPowerAcceleration(-79.36666688342288)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.08, 0.0, 0.01, 0.035))
+            .headingPIDFCoefficients(new PIDFCoefficients(1.0, 0.0, 0.001, 0.03)) //0.035
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.01, 0.0, 0.000005, 0.6, 0.0)) //no idea what t is what it is but everyone uses 0.6
 //            .useSecondaryTranslationalPIDF(true) //staying on path
 //            .useSecondaryHeadingPIDF(true)
 //            .useSecondaryDrivePIDF(true) //progress in path
@@ -37,20 +37,24 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .xVelocity(50.6)
-            .yVelocity(32.6);
+            .xVelocity(52.53851510596088)
+            .yVelocity(35.19910509004368);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint") //is this set?
-            .forwardPodY(1.5)
-            .strafePodX(-4.5)
+            .forwardPodY(1.5) //could be more accurate
+            .strafePodX(-4.5) //could be more accurate
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.99,
+            100,
+            1.0,
+            1.1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
