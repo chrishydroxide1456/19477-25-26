917a7b4801515b1c5f29ee914c31b89d186394c9
chrishydroxide1456 <chrisoh2026@gmail.com>
2025-12-19 23:07:12 -0500

    far 9 ball

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/subsystems/LL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/subsystems/LL.java
@@ -101,7 +101,7 @@ public class LL implements Subsystem {
 
                 // Calculate velocity based on distance (only update when not shooting to prevent mid-shot changes)
                 if (!Outtake.shooting) {
-                    if (distance > 2500.0) {
+                    if (distance > 55.0) {
                         targetVel = 2500.0; //tune later. also tune shot power later
                     } else {
                         double calculatedVel = gettargetVel(distance);
